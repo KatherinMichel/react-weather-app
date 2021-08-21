2fbae9c1e982ec79042c1291e4f2eacc619b3ead
KatherinMichel <82234502+KatherinMichel@users.noreply.github.com>
2021-08-21 22:28:06 +0200

    added the github URL

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
-      
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Katherin Michel and is {" "}
+      <a href="https://github.com/KatherinMichel/react-weather-app"
+      target="_blank">
+        open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
