b399ebd767400b1c55f56482f9cde6e3a73f0d17
KatherinMichel <82234502+KatherinMichel@users.noreply.github.com>
2021-08-29 21:54:21 +0200

    Error fixed

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 export default function FormattedDate(props) {
     let days = [
@@ -13,7 +13,7 @@ export default function FormattedDate(props) {
     let day = days[props.date.getDay()];
     let hours = props.date.getHours();
     if (hours < 10) {
-        hours = `0${minutes}`;
+        hours = `0${hours}`;
     }
     let minutes = props.date.getMinutes();
     if (minutes < 10) {
